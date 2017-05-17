ff96d48700d9ccda7ede4f37e4d1e219e190f4b7
Rebecca DiFede <deathstar@Rebeccas-MacBook-Pro.local>
2017-05-17 18:23:37 -0400

    created game logic, didnt work but will save and keep trying

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,15 +1,15 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[3];
-cardsInPlay.push('king');
-console.log("User flipped " + cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push('king');
-console.log("User flipped " + cardTwo);
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-alert("You found a match!");
-} else if (cardsInPlay[0] !== cardsInPlay[1]) {
-alert("Sorry, try again");
-}
-
-
+var checkForMatch = function (){
+if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log("You found a match!");
+} else {
+console.log("Sorry, try again");
+};
+var flipCard = function (cardId){
+ console.log("User flipped " + cards[cardId]);
+ cardsInPlay.push(cards[cardId]);
+ checkForMatch();
+};
+flipCard(0);
+flipCard(2);
